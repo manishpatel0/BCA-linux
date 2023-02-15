4d406c5434ebc0f9e5a990bed80c2e0ac86840c5
manishpatel0 <mp0000091@gmail.com>
2023-02-15 14:58:12 -0500

    List of List

---
+++ DSA_java/Classroom.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Classroom {
+
+	static class Edge {
+		int src;
+		int dest;
+
+		public Edge(int s, int d) {
+			this.src = s;
+			this.dest = d;
+		}
+	}
+
+	public static void main(String[] args) {
+		int V = 4;
+
+		ArrayList<Edge> graph[] = new ArrayList[V];
+		
+	}
+}
