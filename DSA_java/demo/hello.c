d257cbafe1e639ad37ae3c7994d2c9251071d9f8
manishpatel0 <mp0000091@gmail.com>
2023-02-23 22:02:12 -0500

    DSA in java demo

---
+++ DSA_java/demo/hello.c
@@ -0,0 +1,12 @@
+#include <stdio>
+
+int main(int argc, char const *argv[])
+
+{
+
+	 for (int i = 0; i < count; ++i)
+	 {
+	 	/* code */
+	 }
+	return 0;
+}
---
+++ DSA_java/demo/hellp.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+std::map<key, value> map;
+
+int main(int argc, char const *argv[])
+{
+	printf("%s\n", );
+	return 0;
+}
